fc2964e96e627e24525eaccfd029b9e2a9940ce4
Jędrzej Biedrzycki <jed.biedrzycki@gmail.com>
2020-09-03 09:30:44 +0200

    Add sqlplus connection string

--- oracle-sakila-db/setup.sh
+++ oracle-sakila-db/setup.sh
@@ -1,2 +1,3 @@
 bash oracle/buildDockerImage.sh -v 19.3.0 -e
 docker run -itd --name sakila-oracle -p 1521:1521 -p 5500:5500 -e ORACLE_SID=sakila -e ORACLE_PDB=nsakila -v /home/jb/Workspace/JVM/hibernate-mappings/src/main/resources/oracle/19.3.0/custom_scripts:/opt/oracle/scripts/setup oracle/database:19.3.0-ee
+sqlplus jb/pass@127.17.0.2:1521/sakila
